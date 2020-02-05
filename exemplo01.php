f630638f71b361846522473e881cf732d7a1ec42
= <=>
2020-02-04 21:27:21 -0300

    commit com formulário e validação de resposta

---
+++ exemplo01.php
@@ -0,0 +1,7 @@
+
+<script src="https://www.google.com/recaptcha/api.js" async defer></script>
+<form action="cadastrar.php" method="post">
+	<div class="g-recaptcha" data-sitekey="chave do site"></div>
+	<input type="email" name="inputEmail">
+	<button type="submit">Enviar</button>
+</form>
